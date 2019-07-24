9d0407c1ad485c029ab544882c2f6f2b9a8a9287
Robert Wu <ziwu@microsoft.com>
2019-07-24 12:34:44 -0700

    add oars turning and random objects

--- RemoteBoatRow/Assets/Scripts/Player/BoatManager.cs
+++ RemoteBoatRow/Assets/Scripts/Player/BoatManager.cs
@@ -5,10 +5,20 @@ public class BoatManager : NetworkBehaviour
 {
     private const float TorqueInput = 80;
     private const float ForceInput = 80;
+    private const float RotationInput = 200;
 
     private Rigidbody _rigidbody;
 
     private bool isLeftOarTaken;
+
+    public Transform leftOar;
+    public Transform rightOar;
+
+    public bool isRightOarDirectionForward = false;
+    public bool isLeftOarDirectionForward = false;
+
+    public float rightRotationCounter = 35;
+    public float leftRotationCounter = 35;
 
     public override void OnStartClient()
     {
@@ -36,11 +46,43 @@ public class BoatManager : NetworkBehaviour
     {
         _rigidbody.AddRelativeForce(0, 0, ForceInput * Time.deltaTime * (rowFactor * 2));
         _rigidbody.AddRelativeTorque(Vector3.up * TorqueInput * Time.deltaTime * (rowFactor * 2));
+
+        leftRotationCounter += RotationInput * Time.deltaTime;
+        if (leftRotationCounter >= 70)
+        {
+            isLeftOarDirectionForward = !isLeftOarDirectionForward;
+            leftRotationCounter -= 70;
+        }
+
+        if (isLeftOarDirectionForward)
+        {
+            leftOar.Rotate(0, -1 * RotationInput * Time.deltaTime, 0, Space.Self);
+        }
+        else
+        {
+            leftOar.Rotate(0, RotationInput * Time.deltaTime, 0, Space.Self);
+        }
     }
 
     public void RowRightOar(float rowFactor)
     {
         _rigidbody.AddRelativeForce(0, 0, ForceInput * Time.deltaTime * (rowFactor * 2));
         _rigidbody.AddRelativeTorque(Vector3.up * -TorqueInput * Time.deltaTime * (rowFactor * 2));
+
+        rightRotationCounter += RotationInput * Time.deltaTime;
+        if (rightRotationCounter >= 70)
+        {
+            isRightOarDirectionForward = !isRightOarDirectionForward;
+            rightRotationCounter -= 70;
+        }
+
+        if (isRightOarDirectionForward)
+        {
+            rightOar.Rotate(0, RotationInput * Time.deltaTime, 0, Space.Self);
+        }
+        else
+        {
+            rightOar.Rotate(0, -1 * RotationInput * Time.deltaTime, 0, Space.Self);
+        }
     }
 }
---
+++ RemoteBoatRow/Assets/Scripts/Water/GenerateObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateObjects : MonoBehaviour
+{
+	public GameObject islandPrefab;
+    public GameObject boatPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(islandPrefab, new Vector3(Random.Range(-10.0f, 30.0f), 0, Random.Range(-10.0f, 30.0f)), Quaternion.identity);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(boatPrefab, new Vector3(Random.Range(-10.0f, 30.0f), 0, Random.Range(-10.0f, 30.0f)), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
